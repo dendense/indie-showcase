bd83f616dfaa6dd1e66aa347585b168641c4f92d
dendense <denyfarras@gmail.com>
2022-08-21 19:10:00 +0700

    Updating Brand Name

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import Layout from "../components/Layout";
 
 export default function Index() {
   return (
-    <Layout title="Indie Showcase" description="A Very Indie Showcase">
+    <Layout title="AERONAVY" description="A Very Indie Showcase">
       <div>Hello World!</div>
     </Layout>
   );
