bd260a3f51c15f2b5a9fa0ea902e6355074f9c8e
dendense <denyfarras@gmail.com>
2022-09-06 12:13:34 +0700

    Fix Nav Button

--- components/Nav.js
+++ components/Nav.js
@@ -6,28 +6,36 @@ import { ChevronDownIcon } from "@chakra-ui/icons";
 import Brand from "../components/Brand";
 import Link from "next/link";
 
-const CTA = "Order Now";
+const CTA = "SHOP";
 
 export default function Nav() {
   return (
-    <Flex w="100%" px="6" py="3" align="center">
-      <Box as="Button">
-        <Brand linkto="/" />
-      </Box>
-      <HStack spacing="20px" ml={20}>
+    <Flex>
+      <Flex w="100%" align="center" px="6" py="3">
         <Box as="Button">
-          Catalog
-          <ChevronDownIcon w={5} h={5} />
+          <Brand linkto="/" />
         </Box>
-        <Box as="Button">
-          <Link href="/about">About</Link>
-        </Box>
-        <Box as="Button">Contact</Box>
-      </HStack>
-      <Spacer />
-      <HStack>
-        <Button>{CTA}</Button>
-      </HStack>
+        <HStack spacing="20px" ml={20}>
+          <Box as="Button">
+            Catalog
+            <ChevronDownIcon w={5} h={5} />
+          </Box>
+          <Box as="Button">
+            <Link href="/about">About</Link>
+          </Box>
+          <Box as="Button">Contact</Box>
+        </HStack>
+      </Flex>
+      <Button
+        bgColor="#333333"
+        colorScheme="yellow"
+        borderRadius={0}
+        width="10%"
+        py="9"
+        textColor="white"
+      >
+        {CTA}
+      </Button>
     </Flex>
   );
 }
