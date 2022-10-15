f66b94842d7cc3e9bc98c275005848919ff1e8cb
dendense <denyfarras@gmail.com>
2022-10-15 23:40:57 +0700

    Update Navigation & BigScreen

--- components/BigScreen.js
+++ components/BigScreen.js
@@ -1,10 +1,10 @@
 import * as React from "react";
-import { AspectRatio, Box, Text } from "@chakra-ui/react";
+import { AspectRatio, Box, Text, Flex } from "@chakra-ui/react";
 
 const BigScreen = ({ punchline }) => (
   <AspectRatio maxHeight="300px" ratio={16 / 9}>
     <Box backgroundImage="/drop-hero.jpg" objectFit="cover">
-      <Text color="white" className="brand" fontSize="4xl">
+      <Text color="white" className="brand SmallScreen" fontSize="3xl">
         {punchline}
       </Text>
     </Box>
