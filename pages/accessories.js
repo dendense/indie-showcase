5de83c2ab532da1385c60810929cfc83393acebb
dendense <denyfarras@gmail.com>
2022-10-22 19:41:06 +0700

    Removing Sanity Packages

--- pages/accessories.js
+++ pages/accessories.js
@@ -1,12 +1,9 @@
 import * as React from "react";
-import imageUrlBuilder from "@sanity/image-url";
 
 // Import Local Components
-import { client } from "../lib/sanity";
 import Layout from "../components/Layout";
 import Carousel from "../components/Carousel";
 
-const builder = imageUrlBuilder(client);
 export default function Accessories() {
   return (
     <Layout
--- pages/outwears.js
+++ pages/outwears.js
@@ -1,12 +1,10 @@
 import * as React from "react";
-import imageUrlBuilder from "@sanity/image-url";
 
 // Import Local Components
 import { client } from "../lib/sanity";
 import Layout from "../components/Layout";
 import Carousel from "../components/Carousel";
 
-const builder = imageUrlBuilder(client);
 export default function Outwears({ products }) {
   return (
     <Layout
